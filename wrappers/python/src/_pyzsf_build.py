99771c1001c0edae9268094cb33c7c17a5a143f0
Tjerk Vreeken <tjerk.vreeken@deltares.nl>
2019-10-18 16:39:54 +0200

    Add Python wrapper

---
+++ wrappers/python/src/_pyzsf_build.py
@@ -0,0 +1,144 @@
+import os
+
+from cffi import FFI
+
+
+ffibuilder = FFI()
+
+ffibuilder.cdef(
+    """
+    typedef struct zsf_param_t {
+        double lock_length;
+        double lock_width;
+        double lock_bottom;
+        double num_cycles;
+        double door_time_to_open;
+        double leveling_time;
+        double calibration_coefficient;
+        double symmetry_coefficient;
+        double ship_volume_sea_to_lake;
+        double ship_volume_lake_to_sea;
+        double salinity_lock;
+        double head_sea;
+        double salinity_sea;
+        double temperature_sea;
+        double head_lake;
+        double salinity_lake;
+        double temperature_lake;
+        double flushing_discharge_high_tide;
+        double flushing_discharge_low_tide;
+        double density_current_factor_sea;
+        double density_current_factor_lake;
+        double rtol;
+        double atol;
+    } zsf_param_t;
+
+    typedef struct zsf_results_t {
+        double mass_transport_lake;
+        double salt_load_lake;
+        double discharge_lake_out;
+        double discharge_lake_in;
+        double salinity_lake_in;
+
+        double mass_transport_sea;
+        double salt_load_sea;
+        double discharge_sea_out;
+        double discharge_sea_in;
+        double salinity_sea_in;
+    } zsf_results_t;
+
+    typedef struct zsf_aux_results_t {
+        double z_fraction;
+        double dimensionless_door_open_time;
+        double salinity_lock_1;
+        double salinity_lock_2;
+        double salinity_lock_3;
+        double salinity_lock_4;
+        double mass_transport_lake_1;
+        double mass_transport_lake_2;
+        double mass_transport_lake_4;
+        double mass_transport_sea_2;
+        double mass_transport_sea_3;
+        double mass_transport_sea_4;
+        double volume_lake_in;
+        double volume_lake_out;
+        double volume_sea_in;
+        double volume_sea_out;
+        double volume_lock_at_lake;
+        double volume_lock_at_sea;
+        double t_cycle;
+        double t_open;
+        double t_open_lake;
+        double t_open_sea;
+    } zsf_aux_results_t;
+
+    typedef struct zsf_phase_state_t {
+        double salinity_lock;
+        double saltmass_lock;
+        double head_lock;
+        double volume_ship_in_lock;
+    } zsf_phase_state_t;
+
+    typedef struct zsf_phase_transports_t {
+        double mass_transport_lake;
+        double volume_lake_out;
+        double volume_lake_in;
+        double discharge_lake_out;
+        double discharge_lake_in;
+        double salinity_lake_in;
+
+        double mass_transport_sea;
+        double volume_sea_out;
+        double volume_sea_in;
+        double discharge_sea_out;
+        double discharge_sea_in;
+        double salinity_sea_in;
+    } zsf_phase_transports_t;
+
+    int zsf_initialize_state(const zsf_param_t *p, zsf_phase_state_t *state,
+                              double salinity_lock, double head_lock);
+
+    int zsf_step_phase_1(const zsf_param_t *p, double t_level,
+                          zsf_phase_state_t *state,
+                          zsf_phase_transports_t *results);
+
+    int zsf_step_phase_2(const zsf_param_t *p, double t_open_lake,
+                          zsf_phase_state_t *state,
+                          zsf_phase_transports_t *results);
+
+    int zsf_step_phase_3(const zsf_param_t *p, double t_level,
+                          zsf_phase_state_t *state,
+                          zsf_phase_transports_t *results);
+
+    int zsf_step_phase_4(const zsf_param_t *p, double t_open_sea,
+                          zsf_phase_state_t *state,
+                          zsf_phase_transports_t *results);
+
+    void zsf_param_default(zsf_param_t *p);
+
+    int zsf_calc_steady(const zsf_param_t *p, zsf_results_t *results,
+                         zsf_aux_results_t *aux_results);
+
+    const char * zsf_error_msg(int code);
+
+    const char * zsf_version();
+"""
+)
+
+if os.name == "posix":
+    extra_compile_args = []
+else:
+    extra_compile_args = ["/MD"]
+
+ffibuilder.set_source(
+    "pyzsf._zsf_cffi",
+    '#include "zsf.h"',
+    libraries=["zsf-static"],
+    define_macros=[("ZSF_STATIC", None), ("Py_LIMITED_API", None)],
+    py_limited_api=True,
+    extra_compile_args=extra_compile_args,
+)
+
+
+if __name__ == "__main__":
+    ffibuilder.compile(verbose=True)
